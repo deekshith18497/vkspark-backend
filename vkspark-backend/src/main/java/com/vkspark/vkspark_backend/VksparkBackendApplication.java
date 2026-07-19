ee560ba20fc3867c4fc01ac5b133e48fb866fddc
Deekshith Kumar <dk7374049@gmail.com>
2026-07-19 21:08:45 +0530

    Initial Spring Boot project setup

---
+++ vkspark-backend/src/main/java/com/vkspark/vkspark_backend/VksparkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vkspark.vkspark_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VksparkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VksparkBackendApplication.class, args);
+	}
+
+}
