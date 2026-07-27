86366c53153030307b98083d58a25a4c9e30178f
Deekshith Kumar <dk7374049@gmail.com>
2026-07-27 19:59:25 +0530

    Movie controller is created

--- vkspark-backend/src/main/java/com/vkspark/vkspark_backend/controller/MovieController.java
+++ vkspark-backend/src/main/java/com/vkspark/vkspark_backend/controller/MovieController.java
@@ -1,5 +1,14 @@
 package com.vkspark.vkspark_backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MovieController {
-    
+
+    @GetMapping("/movies")
+    public String getMovies() {
+        return "Welcome to VKSpark Movie API";
+    }
+
 }
