4cb1c7e97e4b8f16c39be002bbc4d833b6650792
Deekshith Kumar <dk7374049@gmail.com>
2026-07-27 22:00:12 +0530

    Connect MovieController with MovieService

--- vkspark-backend/src/main/java/com/vkspark/vkspark_backend/controller/MovieController.java
+++ vkspark-backend/src/main/java/com/vkspark/vkspark_backend/controller/MovieController.java
@@ -1,14 +1,23 @@
 package com.vkspark.vkspark_backend.controller;
 
+import com.vkspark.vkspark_backend.entity.Movie;
+import com.vkspark.vkspark_backend.service.MovieService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
 
-    @GetMapping("/movies")
-    public String getMovies() {
-        return "Welcome to VKSpark Movie API";
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
     }
 
+    @GetMapping("/movies")
+    public List<Movie> getMovies() {
+        return movieService.getAllMovies();
+    }
 }
