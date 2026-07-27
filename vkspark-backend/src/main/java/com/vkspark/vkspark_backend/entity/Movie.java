6c499778750a916f76f5cfb9b62a2aa70bf9e7a8
Deekshith Kumar <dk7374049@gmail.com>
2026-07-27 18:56:04 +0530

    Create Movie entity

--- vkspark-backend/src/main/java/com/vkspark/vkspark_backend/entity/Movie.java
+++ vkspark-backend/src/main/java/com/vkspark/vkspark_backend/entity/Movie.java
@@ -1,5 +1,37 @@
 package com.vkspark.vkspark_backend.entity;
 
-public class Movie {
-    
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "movies")
+public class Movie extends BaseEntity {
+
+    @Column(nullable = false)
+    private String movieName;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column(nullable = false)
+    private Integer duration;
+
+    @Column(nullable = false)
+    private String language;
+
+    @Column(nullable = false)
+    private String genre;
+
+    private String posterUrl;
+
+    @Column(nullable = false)
+    private LocalDate releaseDate;
+
 }
--- vkspark-backend/src/main/java/com/vkspark/vkspark_backend/entity/Role.java
+++ vkspark-backend/src/main/java/com/vkspark/vkspark_backend/entity/Role.java
@@ -2,10 +2,11 @@ package com.vkspark.vkspark_backend.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-
+import java.util.*;
 @Getter
 @Setter
 @Entity
@@ -14,5 +15,6 @@ public class Role extends BaseEntity {
 
     @Column(nullable = false, unique = true)
     private String roleName;
-
+    @OneToMany(mappedBy = "role")
+    private List<User> users;
 }
